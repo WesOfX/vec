232514303d1eafcff5cbceadf0058e55dd6aaeca
Wes <willygoat95@gmail.com>
2017-07-07 03:59:24 -0600

    Add files via upload

---
+++ vec2.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+template<class coord_t>
+struct vec2{
+	coord_t x, y;
+};
+
+template<class coord_t>
+inline vec2<coord_t> operator-(const vec2<coord_t>& rhs){
+	return {-rhs.x, -rhs.y};
+}
+
+template<class coord_t>
+inline vec2<coord_t>& operator+=(
+	vec2<coord_t>& lhs, 
+	const vec2<coord_t>& rhs
+){
+	lhs.x += rhs.x;
+	lhs.y += rhs.y;
+	return lhs;
+}
+
+template<class coord_t>
+inline vec2<coord_t>& operator-=(
+	vec2<coord_t>& lhs,
+	const vec2<coord_t>& rhs
+){
+	lhs.x -= rhs.x;
+	lhs.y -= rhs.y;
+	return lhs;
+}
+
+template<class coord_t>
+inline vec2<coord_t> operator+(
+	const vec2<coord_t>& lhs, 
+	const vec2<coord_t>& rhs
+){
+	return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+template<class coord_t>
+inline vec2<coord_t> operator-(
+	const vec2<coord_t>& lhs, 
+	const vec2<coord_t>& rhs
+){
+	return {lhs.x - rhs.x, lhs.y - rhs.y};
+}
+
+template<class coord_t>
+inline vec2<coord_t> operator*(
+	const vec2<coord_t>& lhs, 
+	coord_t rhs
+){
+	return {lhs.x * rhs, lhs.y * rhs};
+}
+
+template<class coord_t>
+inline vec2<coord_t> operator*(
+	coord_t lhs,
+	const vec2<coord_t>& rhs
+){
+	return {rhs.x * lhs, rhs.y * lhs};
+}
+
+template<class coord_t>
+inline vec2<coord_t>& operator*=(
+	vec2<coord_t>& lhs, 
+	coord_t rhs
+){
+	lhs.x *= rhs;
+	lhs.y *= rhs;
+	return lhs;
+}
+
+template<class coord_t>
+inline vec2<coord_t> operator/(
+	const vec2<coord_t>& lhs, 
+	coord_t rhs
+){
+	return {lhs.x / rhs, lhs.y / rhs};
+}
+
+template<class coord_t>
+inline vec2<coord_t>& operator/=(
+	vec2<coord_t>& lhs,
+	coord_t rhs
+){
+	lhs.x /= rhs;
+	lhs.y /= rhs;
+	return lhs;
+}
+
+template<class coord_t>
+inline bool operator==(
+	const vec2<coord_t>& lhs, 
+	const vec2<coord_t>& rhs
+){
+	return (lhs.x == rhs.x) && (lhs.y == rhs.y);
+}
+
+template<class coord_t>
+inline bool operator!=(
+	const vec2<coord_t>& lhs, 
+	const vec2<coord_t>& rhs
+){
+	return (lhs.x != rhs.x) || (lhs.y != rhs.y);
+}
---
+++ vec3.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+template<class coord_t>
+struct vec3{
+	coord_t x, y, z;
+};
+
+template<class coord_t>
+inline vec3<coord_t> operator-(const vec3<coord_t>& rhs){
+	return {-rhs.x, -rhs.y, -rhs.z};
+}
+
+template<class coord_t>
+inline vec3<coord_t>& operator+=(
+	vec3<coord_t>& lhs, 
+	const vec3<coord_t>& rhs
+){
+	lhs.x += rhs.x;
+	lhs.y += rhs.y;
+	lhs.z += rhs.z;
+	return lhs;
+}
+
+template<class coord_t>
+inline vec3<coord_t>& operator-=(
+	vec3<coord_t>& lhs,
+	const vec3<coord_t>& rhs
+){
+	lhs.x -= rhs.x;
+	lhs.y -= rhs.y;
+	lhs.z -= rhs.z;
+	return lhs;
+}
+
+template<class coord_t>
+inline vec3<coord_t> operator+(
+	const vec3<coord_t>& lhs, 
+	const vec3<coord_t>& rhs
+){
+	return {
+		lhs.x + rhs.x, 
+		lhs.y + rhs.y, 
+		lhs.z + rhs.z
+	};
+}
+
+template<class coord_t>
+inline vec3<coord_t> operator-(
+	const vec3<coord_t>& lhs, 
+	const vec3<coord_t>& rhs
+){
+	return {
+		lhs.x - rhs.x, 
+		lhs.y - rhs.y,
+		lhs.z - rhs.z
+	};
+}
+
+template<class coord_t>
+inline vec3<coord_t> operator*(
+	const vec3<coord_t>& lhs, 
+	coord_t rhs
+){
+	return {
+		lhs.x * rhs, 
+		lhs.y * rhs,
+		lhs.z * rhs
+	};
+}
+
+template<class coord_t>
+inline vec3<coord_t> operator*(
+	coord_t lhs,
+	const vec3<coord_t>& rhs
+){
+	return {
+		rhs.x * lhs, 
+		rhs.y * lhs,
+		rhs.z * lhs
+	};
+}
+
+template<class coord_t>
+inline vec3<coord_t>& operator*=(
+	vec3<coord_t>& lhs, 
+	coord_t rhs
+){
+	lhs.x *= rhs;
+	lhs.y *= rhs;
+	lhs.z *= rhs;
+	return lhs;
+}
+
+template<class coord_t>
+inline vec3<coord_t> operator/(
+	const vec3<coord_t>& lhs, 
+	coord_t rhs
+){
+	return {
+		lhs.x / rhs, 
+		lhs.y / rhs,
+		lhs.z / rhs
+	};
+}
+
+template<class coord_t>
+inline vec3<coord_t>& operator/=(
+	vec3<coord_t>& lhs,
+	coord_t rhs
+){
+	lhs.x /= rhs;
+	lhs.y /= rhs;
+	lhs.z /= rhs;
+	return lhs;
+}
+
+template<class coord_t>
+inline bool operator==(
+	const vec3<coord_t>& lhs, 
+	const vec3<coord_t>& rhs
+){
+	return (lhs.x == rhs.x) 
+	    && (lhs.y == rhs.y)
+	    && (lhs.z == rhs.z);
+}
+
+template<class coord_t>
+inline bool operator!=(
+	const vec3<coord_t>& lhs, 
+	const vec3<coord_t>& rhs
+){
+	return (lhs.x != rhs.x) 
+	    || (lhs.y != rhs.y)
+	    || (lhs.z != rhs.z);
+}
